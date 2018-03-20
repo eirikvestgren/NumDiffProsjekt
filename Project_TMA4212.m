43e3e1fb4226dba89ef0bc4442ccdc3552a567f7
Eirik Vestgren <eirik.vestgren@outlook.com>
2018-03-20 09:52:17 +0100

    Add files via upload

---
+++ Project_TMA4212.m
@@ -0,0 +1,23 @@
+% TMA 4212, project
+% One-way wave equation
+% What is needed: 
+% Finite difference scheme:
+% 
+% The wave equation is hyperbolic.
+% It is linear equation
+%
+% Questions: 
+% 
+% How do we proceed to obtain good boundary condition? 
+% 
+% What does is actually mean that the equation is one-way? 
+% 
+% Absorbing boundary conditions: Not allowing for reflection in the
+% boundary? 
+% 
+% Dispersion relation? 
+
+% Discretization scheme: Leapfrog method, u_one calculated by a simple
+% method, like Euler method. 
+
+% t=0: Dirichlet condition
